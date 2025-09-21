3813ab1292dceca894446c35d44fd15c1ab9b962
Yi Lu <yl18130338@gmail.com>
2025-09-21 15:44:32 -0700

    honestly idk

--- app/(tabs)/landing.jsx
+++ app/(tabs)/landing.jsx
@@ -1,9 +1,9 @@
-import { Button, Image, Linking, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View, useNavigation } from 'react-native';
 import { globalStyles } from '../css';
 
 export default function Landing() {
     const onPress = () => {
-        Linking.openURL("accountCreation")
+        useNavigation.navigate("accountCreation")
     }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -14,13 +14,20 @@ export default function Landing() {
             style={{width: 250, height: 250, borderRadius: 30}}
             />
         <Text style={globalStyles.subheading}>{"\n"}Earthquake Planner App{"\n"} </Text>
-        <Text style={globalStyles.text}>Insert Stats{"\n"} </Text>
+        <Text style={globalStyles.text}>Insert Stats</Text>
         </View>
-        <Button title="Launch!"
+        <TouchableOpacity
         onPress={onPress}
-        color="#3B5249"
-        style={{borderRadius: 10, alignItems: "center"}}
-        />
+        style={{
+        backgroundColor: "#3B5249",
+        borderRadius: 10,
+        padding: 12,
+        marginTop: 20,
+        alignItems: "center",
+        }}
+        >
+        <Text style={{ color: "white", fontSize: 18 }}>Launch!</Text>
+        </TouchableOpacity>
         </View>
     );
 }
