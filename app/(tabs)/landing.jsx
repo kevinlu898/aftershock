0f114257ff9ae308686107763e3585ab5cb25cbb
Yi Lu <yl18130338@gmail.com>
2025-09-21 15:53:45 -0700

    landing page draft like 6 or something

--- app/(tabs)/landing.jsx
+++ app/(tabs)/landing.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Image, Text, TouchableOpacity, View, useNavigation } from 'react-native';
 import { globalStyles } from '../css';
 
@@ -32,34 +31,3 @@ export default function Landing() {
         </View>
     );
 }
-=======
-import { Button, Image, Linking, Text, View } from "react-native";
-import { globalStyles } from "../css";
-
-export default function Landing() {
-  const onPress = () => {
-    Linking.openURL("/accountCreation");
-  };
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={globalStyles.heading}>Aftershock</Text>
-      <View>
-        <Image
-          source={require("../../assets/images/favicon.png")}
-          style={{ width: 250, height: 250, borderRadius: 30 }}
-        />
-        <Text style={globalStyles.subheading}>
-          {"\n"}Earthquake Planner App{"\n"}{" "}
-        </Text>
-        <Text style={globalStyles.text}>Insert Stats{"\n"} </Text>
-      </View>
-      <Button
-        title="Launch!"
-        onPress={onPress}
-        color="#3B5249"
-        style={{ borderRadius: 10, alignItems: "center" }}
-      />
-    </View>
-  );
-}
->>>>>>> 91673df3dc53cf40676380c5032e6fd2b9ce432b
