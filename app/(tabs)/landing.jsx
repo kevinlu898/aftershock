cdb0fe7266fd9a0765da9df34da49315f93fc882
Yi Lu <yl18130338@gmail.com>
2025-09-21 14:40:43 -0700

    landing draft 5

--- app/(tabs)/landing.jsx
+++ app/(tabs)/landing.jsx
@@ -3,7 +3,7 @@ import { globalStyles } from '../css';
 
 export default function Landing() {
     const onPress = () => {
-        Linking.openURL("fish")
+        Linking.openURL("accountCreation")
     }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -11,13 +11,19 @@ export default function Landing() {
         <View>
             <Image
             source={require('../../assets/images/icon.png')}
-            style={{width: 200, height: 200}}
+            style={{width: 200, height: 200, borderRadius: 30}}
             />
+        <br/>
+        <Text style={globalStyles.subheading}>Earthquake Planner App</Text>
+        <br/>
+        <Text style={globalStyles.text}>Insert Stats</Text>
+        <br/>
         </View>
         <Button
         title="Launch!"
         onPress={onPress}
         color="#3B5249"
+        style={{borderRadius: 10}}
         />
         </View>
     );
