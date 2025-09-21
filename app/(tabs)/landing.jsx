cc57343efc9953406484845cec5ace706db339cb
Kevin Lu <kevinlu2882@gmail.com>
2025-09-21 15:53:02 -0700

    fixed conflict

--- app/(tabs)/landing.jsx
+++ app/(tabs)/landing.jsx
@@ -1,44 +1,15 @@
-<<<<<<< HEAD
-import { Image, Text, TouchableOpacity, View, useNavigation } from 'react-native';
-import { globalStyles } from '../css';
-
-export default function Landing() {
-    const onPress = () => {
-        useNavigation.navigate("accountCreation")
-    }
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={globalStyles.heading}>Aftershock</Text>
-        <View>
-            <Image
-            source={require('../../assets/images/favicon.png')}
-            style={{width: 250, height: 250, borderRadius: 30}}
-            />
-        <Text style={globalStyles.subheading}>{"\n"}Earthquake Planner App{"\n"} </Text>
-        <Text style={globalStyles.text}>Insert Stats</Text>
-        </View>
-        <TouchableOpacity
-        onPress={onPress}
-        style={{
-        backgroundColor: "#3B5249",
-        borderRadius: 10,
-        padding: 12,
-        marginTop: 20,
-        alignItems: "center",
-        }}
-        >
-        <Text style={{ color: "white", fontSize: 18 }}>Launch!</Text>
-        </TouchableOpacity>
-        </View>
-    );
-}
-=======
-import { Button, Image, Linking, Text, View } from "react-native";
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  useNavigation,
+} from "react-native";
 import { globalStyles } from "../css";
 
 export default function Landing() {
   const onPress = () => {
-    Linking.openURL("/accountCreation");
+    useNavigation.navigate("accountCreation");
   };
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -51,15 +22,20 @@ export default function Landing() {
         <Text style={globalStyles.subheading}>
           {"\n"}Earthquake Planner App{"\n"}{" "}
         </Text>
-        <Text style={globalStyles.text}>Insert Stats{"\n"} </Text>
+        <Text style={globalStyles.text}>Insert Stats</Text>
       </View>
-      <Button
-        title="Launch!"
+      <TouchableOpacity
         onPress={onPress}
-        color="#3B5249"
-        style={{ borderRadius: 10, alignItems: "center" }}
-      />
+        style={{
+          backgroundColor: "#3B5249",
+          borderRadius: 10,
+          padding: 12,
+          marginTop: 20,
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 18 }}>Launch!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
->>>>>>> 91673df3dc53cf40676380c5032e6fd2b9ce432b
