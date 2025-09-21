8a544b0db426fe2808403a2e9a19ddd3988be278
Yi Lu <yl18130338@gmail.com>
2025-09-21 15:11:52 -0700

    hopefully stops returning 12 errors

--- app/(tabs)/landing.jsx
+++ app/(tabs)/landing.jsx
@@ -13,14 +13,10 @@ export default function Landing() {
             source={require('../../assets/images/favicon.png')}
             style={{width: 250, height: 250, borderRadius: 30}}
             />
-        <br/>
-        <Text style={globalStyles.subheading}>Earthquake Planner App</Text>
-        <br/>
-        <Text style={globalStyles.text}>Insert Stats</Text>
-        <br/>
+        <Text style={globalStyles.subheading}>{"\n"}Earthquake Planner App{"\n"} </Text>
+        <Text style={globalStyles.text}>Insert Stats{"\n"} </Text>
         </View>
-        <Button
-        title="Launch!"
+        <Button title="Launch!"
         onPress={onPress}
         color="#3B5249"
         style={{borderRadius: 10, alignItems: "center"}}
