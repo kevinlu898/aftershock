1ee7b98f6ed6f022d228871cb70b7f2817b8f090
Kevin Lu <kevinlu2882@gmail.com>
2025-09-21 11:56:19 -0700

    basic landing page template created

---
+++ app/(tabs)/dashboard.jsx
@@ -0,0 +1,14 @@
+import { Button, Text, View } from "react-native";
+
+export default function Dashboard() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Welcome, Friend</Text>
+
+      <Text>Your Progress</Text>
+      <Text>What&apos;s Next</Text>
+      <Text>Welcome, Friend</Text>
+      <Button title="During/After the Quake">During/After the Quake</Button>
+    </View>
+  );
+}
