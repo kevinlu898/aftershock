6b8b60891f708c2f67b5a59969b9ccf45ca46eab
Yi Lu <yl18130338@gmail.com>
2025-10-19 13:05:48 -0700

    profile page updates

--- screens/Guide/GuideStyles.jsx
+++ screens/Guide/GuideStyles.jsx
@@ -1,5 +1,5 @@
 import { Platform, StyleSheet } from 'react-native';
-import { colors } from '../../../css';
+import { colors } from '../../css';
 
 export const markdownStyles = {
   body: {
--- screens/Profile/TermsOfService.jsx
+++ screens/Profile/TermsOfService.jsx
@@ -1,13 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
 import { Text, View } from 'react-native';
-import { globalStyles } from '../css';
+import { globalStyles } from '../../css';
 
-export default function UserAgreement() {
+export default function TermsOfService() {
   const navigation = useNavigation();
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.heading}>User Agreement</Text>
+      <Text style={globalStyles.heading}>Terms of Service</Text>
       <Text style={{ marginTop: 12 }}>xyz place holder</Text>
     </View>
   );
